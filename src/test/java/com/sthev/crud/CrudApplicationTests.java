9f97cf27cc03b8ddc6ff8ee67924821999772e3c
Stheven <sthevenalvess@gmail.com>
2024-01-14 17:07:34 -0300

    feat: initial config

---
+++ src/test/java/com/sthev/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sthev.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
