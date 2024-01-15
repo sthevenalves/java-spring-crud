cac73132ba9e99055a2606495097b071d83b35e1
Stheven <sthevenalvess@gmail.com>
2024-01-15 11:24:21 -0300

    feat: create ProductRepository

--- src/main/java/com/sthev/crud/domain/product/Product.java
+++ src/main/java/com/sthev/crud/domain/product/Product.java
@@ -1,5 +1,28 @@
 package com.sthev.crud.domain.product;
 
-public class Product {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "product")
+@Setter @Getter
+@AllArgsConstructor @NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Product {
+	
+	@Id @GeneratedValue(strategy = GenerationType.UUID)
+	private String id;
+	
+	private String name;
+	
+	private Integer price_in_cents;
 }
